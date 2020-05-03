ffad15a153f243d0c391a4ff326ddfce5dba1dd0
jason.lindsay <jasonlindsay87@gmail.com>
2020-05-03 15:34:36 +0100

    Sun 03.05

--- app/src/main/java/com/e/baize/HomeActivity.java
+++ app/src/main/java/com/e/baize/HomeActivity.java
@@ -1,14 +1,28 @@
 package com.e.baize;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
-
+    public static final String PLAYER_ONE_NAME = "com.e.baize.MESSAGE";
+    public static final String PLAYER_TWO_NAME = "com.e.baize.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, scoreboard.class);
+        EditText p1name = findViewById(R.id.p1name);
+        EditText p2name = findViewById(R.id.p2name);
+        String mP1name = p1name.getText().toString();
+        String mP2name = p2name.getText().toString();
+        intent.putExtra(PLAYER_ONE_NAME, mP1name);
+        intent.putExtra(PLAYER_TWO_NAME, mP2name);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/e/baize/scoreboard.java
+++ app/src/main/java/com/e/baize/scoreboard.java
@@ -1,8 +1,10 @@
 package com.e.baize;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class scoreboard extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class scoreboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String mP1name = intent.getStringExtra(HomeActivity.PLAYER_ONE_NAME);
+        String mP2name = intent.getStringExtra(HomeActivity.PLAYER_TWO_NAME);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView tP1Name = findViewById(R.id.p1name);
+        tP1Name.setText(mP1name);
+
+        TextView tP2Name = findViewById(R.id.p2name);
+        tP2Name.setText(mP2name);
     }
 }
